936dc2f4ff1a9e421e59e6739de56fac3b78c1c3
Ali Ghaffari <aligh71@yahoo.com>
2024-01-15 12:48:46 +0330

    create a services folder to manage APIs

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,14 +1,18 @@
 import { useState, useEffect } from "react";
+
 import TableCoin from "../modules/TableCoin";
+import { getCoinList } from "../../services/cryptoApi";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   useEffect(() => {
-    fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en&x_cg_demo_api_key=CG-ckw7zrkfheg6XV1Mh6MC4RqM"
-    )
-      .then((res) => res.json())
-      .then((json) => setCoins(json));
+    const getData = async () => {
+      const res = await fetch(getCoinList());
+      const json = await res.json();
+      setCoins(json);
+    };
+    
+    getData();
   }, []);
   return (
     <div>
