a7b33f009ee35afde068c6f1fa903a3215da6b46
Ali Ghaffari <aligh71@yahoo.com>
2024-01-16 13:30:29 +0330

    changing currency related to data

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -5,7 +5,7 @@ import chartDown from "../../assets/chart-down.svg";
 
 import styles from "./TableCoin.module.css";
 
-function TableCoin({ coins, isLoading }) {
+function TableCoin({ coins, isLoading, currency }) {
   return (
     <div className={styles.container}>
       {isLoading ? (
@@ -24,7 +24,7 @@ function TableCoin({ coins, isLoading }) {
           </thead>
           <tbody>
             {coins.map((coin) => (
-              <TableRow coin={coin} key={coin.id} />
+              <TableRow coin={coin} key={coin.id} currency={currency} />
             ))}
           </tbody>
         </table>
@@ -35,16 +35,15 @@ function TableCoin({ coins, isLoading }) {
 
 export default TableCoin;
 
-const TableRow = ({
-  coin: {
+const TableRow = ({ coin, currency }) => {
+  const {
     name,
     image,
     symbol,
     current_price,
     price_change_percentage_24h: price_change,
     total_volume,
-  },
-}) => {
+  } = coin;
   return (
     <tr>
       <td>
@@ -54,7 +53,10 @@ const TableRow = ({
         </div>
       </td>
       <td>{name}</td>
-      <td>{current_price.toLocaleString()}</td>
+      <td>
+        {currency == "usd" ? "$" : currency === "eur" ? "€" : "¥"}
+        {current_price.toLocaleString()}
+      </td>
       <td className={price_change > 0 ? styles.success : styles.error}>
         {price_change.toFixed(2)}%
       </td>
