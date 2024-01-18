d06c0989bfd15d094d1a19ea2fdffc3a3e2a9afb
Ali Ghaffari <aligh71@yahoo.com>
2024-01-19 00:11:06 +0330

    add cleanup function

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -15,10 +15,14 @@ function HomePage() {
   useEffect(() => {
     setIsLoading(true);
     const getData = async () => {
-      const res = await fetch(getCoinList(page, currency));
-      const json = await res.json();
-      setCoins(json);
-      setIsLoading(false);
+      try {
+        const res = await fetch(getCoinList(page, currency));
+        const json = await res.json();
+        setCoins(json);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
     };
 
     getData();
