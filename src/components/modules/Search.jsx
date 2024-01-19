9f84c34b6b0a7124b12f38533aa9f2382711fbf1
Ali Ghaffari <aligh71@yahoo.com>
2024-01-19 11:35:43 +0330

    styling the SearchBox & debugging the Search component after styling

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -3,6 +3,8 @@ import { RotatingLines } from "react-loader-spinner";
 
 import { searchCoin } from "../../services/cryptoApi";
 
+import styles from "./Search.module.css";
+
 function Search({ currency, setCurrency }) {
   const [text, setText] = useState("");
   const [coins, setCoins] = useState([]);
@@ -13,7 +15,7 @@ function Search({ currency, setCurrency }) {
     setCoins([]);
 
     if (!text) {
-      setIsLoading(false)
+      setIsLoading(false);
       return;
     }
 
@@ -42,7 +44,7 @@ function Search({ currency, setCurrency }) {
     return () => controller.abort();
   }, [text]);
   return (
-    <div>
+    <div className={styles.searchBox}>
       <input
         type="text"
         placeholder="Search"
@@ -54,24 +56,26 @@ function Search({ currency, setCurrency }) {
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
-      <div>
-        {isLoading && (
-          <RotatingLines
-            width="50px"
-            height="50px"
-            strokeWidth="2"
-            strokeColor="#3874ff"
-          />
-        )}
-        <ul>
-          {coins.map((coin) => (
-            <li key={coin.id}>
-              <img src={coin.thumb} alt={coin.name} />
-              <p>{coin.name}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {(!!coins.length || isLoading) && (
+        <div className={styles.searchResult}>
+          {isLoading && (
+            <RotatingLines
+              width="50px"
+              height="50px"
+              strokeWidth="2"
+              strokeColor="#3874ff"
+            />
+          )}
+          <ul>
+            {coins.map((coin) => (
+              <li key={coin.id}>
+                <img src={coin.thumb} alt={coin.name} />
+                <p>{coin.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
